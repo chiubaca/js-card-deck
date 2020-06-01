42cb5e32d456bc9a6fb38231b84bc46dad60e852
Alex Chiu <alexchiu11@gmail.com>
2020-06-01 16:16:48 +0100

    Ordered default to deck to "Big 2" styles. Implemented show top and bottom cards in deck.

--- src/classes/Deck.ts
+++ src/classes/Deck.ts
@@ -7,8 +7,8 @@ export class Deck {
     this.deck = [];
 
     // generate the deck of cards
-    const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
-    const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+    const suits = ['Diamonds', 'Clubs', 'Hearts', 'Spades'];
+    const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
     for (let suit in suits) {
       for (let value in values) {
         this.deck.push({
@@ -20,16 +20,38 @@ export class Deck {
   }
 
   /**
-   * returns an un-shuffled deck of cards
+   * returns the deck of cards in it's current state
    */
   public get getDeck(): Card[] {
     return this.deck
   }
 
   /**
-   * returns an shuffled deck of cards
+   * Returns the first card in the deck and moves it last .This changes the state of the deck
    */
-  public get getShuffledDeck(): Card[] {
+  public showTopCard(): Card {
+    let deck = this.deck;
+    let topCard = deck.shift()
+    let newDeck = [...deck, topCard]
+    this.deck = newDeck;
+    return topCard
+  }
+
+  /**
+   * Returns the last card in the deck and moves it first. This changes the state of the deck
+   */
+  public showBottomCard(): Card {
+    let deck = this.deck;
+    let bottomCard = deck.pop()
+    let newDeck = [bottomCard, ...deck]
+    this.deck = newDeck;
+    return bottomCard
+  }
+
+  /**
+   * Returns an shuffled deck of cards. This changes the state of the deck
+   */
+  public shuffledDeck(): Card[] {
     let deck = this.deck
     let currentIndex = deck.length;
     let temporaryValue, randomIndex;
@@ -44,8 +66,8 @@ export class Deck {
       deck[currentIndex] = deck[randomIndex];
       deck[randomIndex] = temporaryValue;
     }
-
-    return deck
+    this.deck = deck;
+    return this.deck
   }
 
 }
